a72fbb0f8a50f1610746ee4481f1cba5bd293217
Tokiwa-17 <2872444509@qq.com>
2023-03-30 16:04:13 +0800

    sort key-value

--- proj1-main/src/sort.go
+++ proj1-main/src/sort.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
 	"log"
 	"os"
+	"sort"
 )
 
 func main() {
@@ -12,5 +15,37 @@ func main() {
 		log.Fatalf("Usage: %v inputfile outputfile\n", os.Args[0])
 	}
 
+	bytes_, err := os.ReadFile(os.Args[1])
+	if os.IsNotExist(err) {
+		log.Fatalf("File not exist\n")
+	} else if err != nil {
+		log.Fatalf("File access error\n")
+	}
+	log.Printf("%s open success, length: %d\n", os.Args[1], len(bytes_))
+	var pairNum int = len(bytes_) / 100
+	mp := make(map[uint16]string, pairNum)
+	idx := 0
+	for ; idx < pairNum; idx++ {
+		key := bytes_[idx*100 : (idx+1)*100][:10]
+		val := bytes_[idx*100 : (idx+1)*100][10:]
+		var j uint16
+		buf := bytes.NewReader(key)
+		binary.Read(buf, binary.LittleEndian, &j)
+		mp[j] = string(val)
+	}
+
+	// for key, val := range mp {
+	// 	log.Println(key, val)
+	// }
+	var key_arr []uint16
+	for k := range mp {
+		key_arr = append(key_arr, k)
+	}
+	sort.Slice(key_arr, func(i, j int) bool {
+		return key_arr[i] < key_arr[j]
+	})
+	for _, k := range key_arr {
+		log.Println(k, mp[k])
+	}
 	log.Printf("Sorting %s to %s\n", os.Args[1], os.Args[2])
 }
